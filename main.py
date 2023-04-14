0c7fd571e4a3b84ba2769ae054c9eef80713c511
geertruiherremans <geertrui.herremans@gmail.com>
2023-04-14 21:14:24 +0200

    first attempt at GAM

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ Tariff plan construction using:
 - Gradient boosting
 """
 import pandas as pd
+from pygam import PoissonGAM, s, f
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
 data = pd.read_csv('data.csv')
-dataSev = data[data.nbrtotc!=0]
+data['sex'] = data.sexp == 'Male'
+data.sex = data['sex'].astype(int)
+y = data.nbrtotc.to_numpy()
+X = data[['AGEPH','sex']].to_numpy()
+gamTest = PoissonGAM(s(0)+f(1)).fit(X,y,exposure=data.duree)
+gamTest.summary()
