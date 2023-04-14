7f7d28fdd574aa59e25a0dc049211351a2b09918
geertruiherremans <geertrui.herremans@gmail.com>
2023-04-14 23:08:26 +0200

    GAM first attempt

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ Tariff plan construction using:
 - Gradient boosting
 """
 import pandas as pd
-from pygam import PoissonGAM, s, f
+from pygam import PoissonGAM, s, f, te
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 import geopandas as gpd
+from scipy.interpolate import interp1d
 
-data = pd.read_csv('data.csv')
-data['sex'] = data.sexp == 'Male'
-data.sex = data['sex'].astype(int)
+
+data = pd.read_csv('dataFact.csv')
 y = data.nbrtotc.to_numpy()
-X = data[['AGEPH','sex']].to_numpy()
-gamTest = PoissonGAM(s(0)+f(1)).fit(X,y,exposure=data.duree)
+X = data[['ageph','sexp','coverp','fuelc','lat','long']].to_numpy()
+gamTest = PoissonGAM(s(0)+f(1)+f(2)+f(3)+te(4,5)).fit(X,y,exposure=data.duree)
 gamTest.summary()
+
+postalData = pd.read_excel('inspost.xls')
+postalData = postalData.sort_values(by='CODPOSS')
+coord = postalData[['LAT','LONG']].to_numpy()
+XX = np.concatenate((np.zeros((coord.shape[0],4)),coord),axis=1)
+YY = gamTest.predict(XX)
+postalData['YY'] = YY
+postalData = postalData.rename(columns={'CODPOSS':'POSTCODE'})
+shp = gpd.read_file(r'./shape_file/npc96_region_Project1.shp')
+shp = shp.sort_values(by='POSTCODE')
+shp.YY = shp.YY.fillna(0)
+
+shp.plot(column='duree',cmap='jet',legend=True)
--- readData.py
+++ readData.py
@@ -7,10 +7,81 @@ Converting input data 'Assignment.csv' to 'data.csv' (used in main)
 import pandas as pd
 import numpy as np
 
+
+def split_to_numeric(x):
+    if x == 'Once': return 1
+    if x == 'Twice': return 2
+    if x == 'Thrice': return 3
+    return 0
+
+
+def agec_to_numeric(x):
+    if x == '2-5': return 1
+    if x == '6-10': return 2
+    if x == '>10': return 3
+    return 0
+
+
+def powerc_to_numeric(x):
+    if x == '66-110': return 1
+    if x == '>110': return 2
+    return 0
+
+
+def sexp_to_numeric(x):
+    if x == 'Male': return 1
+    return 0
+
+
+def fuelc_to_numeric(x):
+    if x == 'Gasoil': return 1
+    return 0
+
+
+def usec_to_numeric(x):
+    if x == 'Professional': return 1
+    return 0
+
+
+def fleetc_to_numeric(x):
+    if x == 'Yes': return 1
+    return 0
+
+
+def sportc_to_numeric(x):
+    if x == 'Yes': return 1
+    return 0
+
+
+def coverp_to_numeric(x):
+    if x == 'MTPL+': return 1
+    if x == 'MTPL+++': return 2
+    return 0
+
+
 data = pd.read_csv('Assignment.csv')
 postalData = pd.read_excel('inspost.xls')
-data = pd.merge(data,postalData)
-data['chargper'] = np.where(data['nbrtotc']>0,data.chargtot / data.nbrtotc,0)
+data = pd.merge(data, postalData)
+data['chargper'] = np.where(data['nbrtotc'] > 0, data.chargtot / data.nbrtotc, 0)
 
-data.to_csv(r'./data.csv', encoding='utf-8',index=False)
+dataFact = pd.DataFrame()
+dataFact['ageph'] = data.AGEPH
+dataFact['duree'] = data.duree
+dataFact['lnexpo'] = data.lnexpo
+dataFact['nbrtotc'] = data.nbrtotc
+dataFact['chargtot'] = data.chargtot
+dataFact['chargper'] = data.chargper
+dataFact['agecar'] = data.agecar.apply(agec_to_numeric)
+dataFact['sexp'] = data.sexp.apply(sexp_to_numeric)
+dataFact['fuelc'] = data.fuelc.apply(fuelc_to_numeric)
+dataFact['split'] = data.split.apply(split_to_numeric)
+dataFact['usec'] = data.usec.apply(usec_to_numeric)
+dataFact['fleetc'] = data.fleetc.apply(fleetc_to_numeric)
+dataFact['sportc'] = data.sportc.apply(sportc_to_numeric)
+dataFact['coverp'] = data.coverp.apply(coverp_to_numeric)
+dataFact['powerc'] = data.powerc.apply(powerc_to_numeric)
+dataFact['lat'] = data.LAT
+dataFact['long'] = data.LONG
 
+data.to_csv(r'./data.csv', encoding='utf-8', index=False)
+dataFact.to_csv(r'./dataFact.csv', encoding='utf-8', index=False)
